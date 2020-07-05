7e62dff1af745009b614dd200bc52fc9f0caff91
Aleksandar Ristic <storiaca@gmail.com>
2020-07-05 13:28:39 +0200

    useEffect and HTTP Request

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import React from "react";
-
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Header from "./components/ui/Header";
 import "./App.css";
 
 function App() {
+  const [items, setItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchItems = async () => {
+      const result = await axios.get(
+        `https://www.breakingbadapi.com/api/characters`
+      );
+      console.log(result.data);
+      setItems(result.data);
+      setIsLoading(false);
+    };
+    fetchItems();
+  }, []);
+
   return (
     <div className="container">
-      <h2>Hello</h2>
+      <Header />
     </div>
   );
 }
