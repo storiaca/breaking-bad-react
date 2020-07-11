5f19dff74592122137a20294bde0119b40a823d5
Aleksandar Ristic <storiaca@gmail.com>
2020-07-11 15:58:30 +0200

    Spinner

--- src/components/characters/CharacterGrid.js
+++ src/components/characters/CharacterGrid.js
@@ -1,9 +1,10 @@
 import React from "react";
 import CharacterItem from "./CharacterItem";
+import Spinner from "../ui/Spinner";
 
 const CharacterGrid = ({ isLoading, items }) => {
   return isLoading ? (
-    <h3>Loading...</h3>
+    <Spinner />
   ) : (
     <section className="cards">
       {items.map((item) => (
